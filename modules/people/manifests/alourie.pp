df612358ba5e0f1da6cc7ccfe5f7b9e02243cbc1
Alex Lourie <alexlourie@Alexs-MacBook-Pro.local>
2015-01-25 01:11:21 +1030

    initial

---
+++ modules/people/manifests/alourie.pp
@@ -0,0 +1,20 @@
+class people::alourie {
+
+  include macvim
+  include zsh
+  include atom
+  
+  include brewcask
+
+  package { 'google-chrome': provider => 'brewcask' }
+  package { 'firefox': provider => 'brewcask' }
+  package { 'intellij-idea': provider => 'brewcask' }
+  package { 'slack': provider => 'brewcask' }
+  package { 'virtualbox': provider => 'brewcask' }
+  package { 'vagrant': provider => 'brewcask' }
+  package { 'java': provider => 'brewcask' }
+
+
+	
+
+}
